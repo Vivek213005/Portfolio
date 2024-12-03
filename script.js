1c087d8c2c424304650e604c62600fb9f7982d89
Vivek Kumar <vivekk_213005@saitm.org>
2024-12-03 23:01:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("linkedin-link").href = "https://www.linkedin.com/in/vivek-kumar-1b9b70239/";
+document.getElementById("github-link").href = "https://github.com/Vivek213005";
+ 
